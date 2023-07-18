9d3e4775f424ef74ef5513af6759ed7ad269bbba
jose zambrano <jose.zambrano@sinapsiscorp.com>
2023-07-18 13:02:59 -0500

    added id_user

--- user/signup.js
+++ user/signup.js
@@ -9,7 +9,7 @@ module.exports.handler = async (event) => {
         if (!isValid)
             return sendResponse(400, { message: 'Invalid input' })
 
-        const { email, password } = JSON.parse(event.body)
+        const { email, password, id_user } = JSON.parse(event.body)
         const { user_pool_id } = process.env
         const params = {
             UserPoolId: user_pool_id,
@@ -22,11 +22,12 @@ module.exports.handler = async (event) => {
                 {
                     Name: 'email_verified',
                     Value: 'true'
-                }],
+                }
+            ],
             MessageAction: 'SUPPRESS'
         }
         const response = await cognito.adminCreateUser(params).promise();
-        //console.log(response.User.Attributes)
+
         if (response.User) {
             const paramsForSetPass = {
                 Password: password,
@@ -35,17 +36,23 @@ module.exports.handler = async (event) => {
                 Permanent: true
             };
             await cognito.adminSetUserPassword(paramsForSetPass).promise()
+
+            // Asociar el ID personalizado al atributo 'sub' del usuario
+            const paramsForUpdateAttributes = {
+                UserPoolId: user_pool_id,
+                Username: email,
+                UserAttributes: [
+                    {
+                        Name: 'custom:id_user',
+                        Value: id_user
+                    }
+                ]
+            };
+            await cognito.adminUpdateUserAttributes(paramsForUpdateAttributes).promise();
         }
-        //return sendResponse(200, { message: 'User registration successful' })
-        //return sendResponse(200, { 
-        //    Password: password,
-        //    UserPoolId: user_pool_id,
-        //    Username: email,
-        //    Permanent: true 
-        //})
-        return sendResponse(200, response.User.Attributes )
-    }
-    catch (error) {
+
+        return sendResponse(200, response.User.Attributes)
+    } catch (error) {
         const message = error.message ? error.message : 'Internal server error'
         return sendResponse(500, { message })
     }
